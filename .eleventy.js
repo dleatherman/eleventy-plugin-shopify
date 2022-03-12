174a882c038c637d7ccc7d42a98dba0a7c7d5aee
Dan Leatherman <dleatherman@gmail.com>
2022-03-12 17:53:45 -0500

    upgrade to 2.0.0-canary.3

--- .eleventy.js
+++ .eleventy.js
@@ -12,46 +12,46 @@ const getShopifyContent = async (config) => {
   // CHECK CONFIG VALIDATION
 
   console.log(chalk.yellow.bold(`SHOPIFY:GETTING SHOP INFO`))
-  // console.log(chalk.yellow.bold(`SHOPIFY:GETTING PRODUCTS`))
-  // console.log(chalk.yellow.bold(`SHOPIFY:GETTING COLLECTIONS`))
+  console.log(chalk.yellow.bold(`SHOPIFY:GETTING PRODUCTS`))
+  console.log(chalk.yellow.bold(`SHOPIFY:GETTING COLLECTIONS`))
   console.log(chalk.yellow.bold(`SHOPIFY:GETTING PAGES`))
-  // console.log(chalk.yellow.bold(`SHOPIFY:GETTING ARTICLES`))
+  console.log(chalk.yellow.bold(`SHOPIFY:GETTING ARTICLES`))
 
   const shop = await getShopInfo()
-  // const products = await getAllProducts()
-  // const collections = await getAllCollections()
+  const products = await getAllProducts()
+  const collections = await getAllCollections()
   const pages = await getAllPages()
-  // const articles = await getAllArticles()
+  const articles = await getAllArticles()
 
   console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${shop.name.toUpperCase()} INFO`))
-  // console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${products.length} PRODUCT${products.length > 1 || products.length == 0 ? 'S' : ''}`))
-  // console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${collections.length} COLLECTIONS`))
+  console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${products.length} PRODUCT${products.length > 1 || products.length == 0 ? 'S' : ''}`))
+  console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${collections.length} COLLECTIONS`))
   console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${pages.length} PAGE${pages.length > 1 || pages.length == 0 ? 'S' : ''}`))
-  // console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${articles.length} ARTICLE${articles.length > 1 || articles.length == 0 ? 'S' : ''}`))
+  console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY RETRIEVED ${articles.length} ARTICLE${articles.length > 1 || articles.length == 0 ? 'S' : ''}`))
 
-  // console.log(chalk.yellow.bold(`SHOPIFY:MAPPING PRODUCTS TO COLLECTIONS`))
+  console.log(chalk.yellow.bold(`SHOPIFY:MAPPING PRODUCTS TO COLLECTIONS`))
 
-  // collections.map(collection => {
-  //   if (collection.products.length > 0) {
-  //     return collection.products.map(collectionProduct => {
-  //       const foundProduct = products.find(product => {
-  //         return product.id === collectionProduct.id
-  //       })
-  //       return foundProduct
-  //     })
-  //   } else {
-  //     return collection
-  //   }
-  // })
+  collections.map(collection => {
+    if (collection.products.length > 0) {
+      return collection.products.map(collectionProduct => {
+        const foundProduct = products.find(product => {
+          return product.id === collectionProduct.id
+        })
+        return foundProduct
+      })
+    } else {
+      return collection
+    }
+  })
 
-  // console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY MAPPED PRODUCTS TO COLLECTIONS`))
+  console.log(chalk.greenBright.bold(`SHOPIFY:SUCCESSFULLY MAPPED PRODUCTS TO COLLECTIONS`))
 
   return {
     shop: shop,
-    // products: products,
-    // collections: collections,
+    products: products,
+    collections: collections,
     pages: pages,
-    // articles: articles,
+    articles: articles,
   };
 };
 
